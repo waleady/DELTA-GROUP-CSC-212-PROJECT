904e578e79283520865cb4290c3d33adfb236849
MukhtarUmarGhaly <67170906+MukhtarUmarGhaly@users.noreply.github.com>
2021-07-20 21:07:09 +0100

    Add files via upload

---
+++ head.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="asset/css/head.css"> 
+    <link rel="stylesheet" type="text/css" href="asset/bootstrap/css/bootstrap.min.css">
+    <script src="asset/bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<header>
+      <h1><strong> Delta Pharmacy</strong></h1>
+                <h5>your number one online pharmacy</h5>
+    
+    <hr>
+    <ul>
+    <li class="active"><a href="dashboard.php">HOME</a></li>
+      <li><a href="consultancy.php">CONSULTANCY</a></li>
+      <li><a href="pharmacy.php">PHARMACY</a></li>
+      <li><a href="emergency.php">EMERGENCY SERVICES</a></li>
+      
+    </ul>
+    
+</header>
+ <section id="main-body">
+   <div class="container">
+     <div class="row">
+      <div class="col-md-12">
+  <h2>DELTA PHARMACY</h2>
+  <p style="text-align: justify-all;">Delta pharmacy the first online drugs chemist with highly trained professional chemists always ready to dispence and deliver your requested drugs to your designated location with precission according to your order. We also offer consultancy services,Emergency services,and also medical health advice.</p>
+   
+      </div>
+       
+     </div>
+   </div>
+ </section>
+
+ 
+ 
+  
+</body>
+</html>
