3ca8280005226b4faadb0e938f39ebad0c096ef7
bellosalihu <87807476+bellosalihu@users.noreply.github.com>
2021-07-22 11:02:16 +0100

    Create pharmacy.php

---
+++ pharmacy.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>pharmacy</title>
+    <link rel="stylesheet" type="text/css" href="pharm.css"> 
+    <link rel="stylesheet" type="text/css" href="asset/bootstrap/css/bootstrap.min.css">
+    <script src="asset/bootstrap/js/bootstrap.min.js"></script>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Viaoda+Libre&display=swap" rel="stylesheet">
+
+    
+</head>
