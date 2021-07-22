79f54d61521267570661f7c5e9eec2d0dc9d4c51
Roberttsegba <87038133+Roberttsegba@users.noreply.github.com>
2021-07-22 20:27:19 +0100

    robert tsegba fcp/csc/18/1037

---
+++ footer.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <link rel="stylesheet" type="text/css" href="asset/css/stylee.css"> 
+    <link rel="stylesheet" type="text/css" href="asset/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="asset/fontawesome/css/all.min.css">
+    <script src="asset/bootstrap/js/bootstrap.min.js"></script>
+    <script src="asset/fontawesome/js/all.min.js"></script>
+    <style type="text/css">
+    	footer{
+    		background-color: #e6e600;
+    		height: 160px;
+    		width: 100%;
+            margin-top: 100px;
+            margin-bottom: 0;
+    	}
+    	footer img{
+    		margin-top: 2.5px;
+    	}
+    	footer p{
+    		font-size: 15px;
+    	}
+    	footer a{
+    		margin-top: 50px;
+    		font-size: 40px;
+    		color: #009900;
+    	}
+    </style>
+</head>
+<body>
+<footer>
+	<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<img src="asset/img/1.jpg" width="100%" height="70%">
+					</div>
+					<div class="col-md-4">
+						<p>Delta pharmacy the first online drugs chemist with highly trained professional chemists always ready to dispence and deliver your requested drugs to your designated location with precission according to your order.</p>
+					</div>
+					<div class="col-md-4">
+						<a href=""><i class="fab fa-facebook-f"></i></a>
+						<a href=""><i class="fab fa-whatsapp"></i></a>
+						<a href=""><i class="fab fa-twitter"></i></a>
+						<a href=""><i class="fab fa-instagram"></i></a>
+					</div>
+				</div>
+				
+		</div>
+		
+	</div>
+</footer>
+</body>
+</html>
